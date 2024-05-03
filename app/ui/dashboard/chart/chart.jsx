800393e9a2ece6669c0274dc4bf351d07482d3b3
nthapa000 <nishantthapa0000@gmail.com>
2024-05-03 14:27:16 +0530

    #3 Dashboard/Cards

---
+++ app/ui/dashboard/chart/chart.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './chart.module.css'
+
+const Chart = () => {
+  return (
+    <div className={styles.container}>Charts</div>
+  )
+}
+
+export default Chart
---
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './transactions.module.css'
+
+const Transactions = () => {
+  return (
+    <div className={styles.container}>Transactions</div>
+  )
+}
+
+export default Transactions
