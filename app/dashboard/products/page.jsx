5b2332197ac5a0819602b7be1ef2ff7d6becfd39
nthapa000 <nishantthapa0000@gmail.com>
2024-05-05 01:36:10 +0530

    #8 User and Products page along with pagination component

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,8 +1,67 @@
 import React from 'react'
+import Link from 'next/link'
+import Search from '@/app/ui/dashboard/search/search'
+import Image from 'next/image'
+import styles from '@/app/ui/dashboard/products/products.module.css'
+import Pagination from '@/app/ui/dashboard/pagination/pagination'
 
 const ProductsPage = () => {
   return (
-    <div>ProductsPage</div>
+    <div className={styles.container}>
+    <div className={styles.top}>
+      <Search placeholder={"Search for a product..."} />
+      <Link href="/dashboard/products/add">
+        <button className={styles.addButton}>Add New</button>
+      </Link>
+    </div>
+    <table className={styles.table}>
+      <thead>
+        <tr>
+          <td>Title</td>
+          <td>Description</td>
+          <td>Price</td>
+          <td>Created at</td>
+          <td>Stock</td>
+          <td>Action</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <div className={styles.product}>
+              <Image
+                src="/noproduct.jpg"
+                alt=""
+                width={40}
+                height={40}
+                className={styles.productImage}
+              />
+              Baweee Iphonee
+            </div>
+          </td>
+          <td>Description</td>
+          <td>$1123</td>
+          <td>13.01.2024</td>
+          <td>75</td>
+          <td>
+            <div className={styles.buttons}>
+              <Link href="/">
+                <button className={`${styles.button} ${styles.view}`}>
+                  {" "}
+                  Add
+                </button>
+              </Link>
+              <button className={`${styles.button} ${styles.delete}`}>
+                {" "}
+                Delete
+              </button>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <Pagination />
+  </div>
   )
 }
 
--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react";
+import styles from "@/app/ui/dashboard/users/user.module.css";
+import Search from "@/app/ui/dashboard/search/search";
+import Link from "next/link";
+import Image from "next/image";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
 
+// we will have search will be functional
 const UserPage = () => {
   return (
-    <div>UserPage</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder={"Search for a user..."} />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created At</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+              </div>
+            </td>
+            <td>Jogn@gmail.com</td>
+            <td>12.01.2024</td>
+            <td>Admin</td>
+            <td>active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    {" "}
+                    Add
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  {" "}
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
+};
 
-export default UserPage
+export default UserPage;
